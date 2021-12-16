66342db81d26dd2145969893cd578d86a3879ff3
Paul Scheikl <paul.scheikl@kit.edu>
2021-12-16 20:42:40 +0100

    Using static registration now, no marker on cam. 	Works :)

--- scripts/register.py
+++ scripts/register.py
@@ -9,74 +9,19 @@ from sensor_msgs.msg import PointCloud2
 import tf2_ros
 import geometry_msgs.msg
 import time
-
-# from std_msgs.msg import String
-# import std_msgs.msg as std_msgs
-# import sensor_msgs.msg as sensor_msgs
-# from nav_msgs.msg import Odometry
-# import sensor_msgs.point_cloud2 as pc2
-
-
 import utils
 
 class RegistrationManager:
     def __init__(self):
         rospy.init_node('zedm_art_registration_node', anonymous=True)
 
-        #self.broadcaster = tf2_ros.TransformBroadcaster()
-        #t = geometry_msgs.msg.TransformStamped()
-
-        #t.header.frame_id = "body6"
-        #t.child_frame_id = "inverseART"
-
-        #rate = rospy.Rate(10.0)
-
-
-        self.art_listener = rospy.Subscriber("/art/body6/pose", PoseStamped, self.art_callback)
         self.pointcloud_listener = rospy.Subscriber("/zedm/zed_node/point_cloud/cloud_registered", PointCloud2, self.point_cloud_callback)
-        self.pointcloud_publisher = rospy.Publisher('no_odometry_point_cloud', PointCloud2, queue_size=1)
-        self.art_transformation = np.zeros((4,4))
-        self.art_translation = np.zeros((3,3))
-        self.art_rotation = np.zeros((3,1))
+        self.pointcloud_publisher = rospy.Publisher('art_point_cloud', PointCloud2, queue_size=1)
 
-        #self.zed_camera_odometry_listener = rospy.Subscriber("/zedm/zed_node/odom", Odometry, self.odometry_callback)
-        #self.odometry_transformation = np.zeros((4,4))
 
     def run(self):
         rospy.spin()
-
-
-
-    def art_callback(self, data):
-        """From marker space to ART space"""
-
-        
-
-        rot = data.pose.orientation
-        pos = data.pose.position
-        self.art_transformation, self.art_rotation, self.art_translation = to_affine_transformation_matrix(pos, rot)
-
-        b1,b2,b3 = [1,0,0], [0,1,0], [0,0,1]
-
-        rotation_from_quat = Rotation.from_quat([rot.x, rot.y,rot.z,rot.w]).as_matrix()
-
-        c1 = rotation_from_quat.dot(b1)
-        c2 = rotation_from_quat.dot(b2)
-        c3 = rotation_from_quat.dot(b3)
-
-        rot_mat = np.column_stack((c1,c2,c3))
    
-       
-
-
-    # def odometry_callback(self, data)-> None:
-    #     """From odometry (fake world) space to depth camera space.
-    #
-    #     Removes the camera motion offset.
-    #     """
-    #     rot = data.pose.pose.orientation
-    #     pos = data.pose.pose.position
-    #     self.odometry_transformation = self.to_inverse_affine_transformation_matrix(pos, rot)
 
     def point_cloud_callback(self, data) -> None:
         time1 = time.time()
@@ -84,7 +29,9 @@ class RegistrationManager:
 
         extended_pointcloud_array = np.c_[pointcloud_array, np.ones(pointcloud_array.shape[0])]
         
-        transformed_pointcloud_array = transform_pointcloud_array(self.art_transformation, extended_pointcloud_array)
+        T = get_transformation()
+        I = get_identity_transformation()
+        transformed_pointcloud_array = transform_pointcloud_array(T, extended_pointcloud_array)
 
         transformed_pointcloud = utils.xyz_array_to_pointcloud2(transformed_pointcloud_array[:, :3])
         
@@ -102,52 +49,84 @@ def transform_pointcloud_array(transformation, pointcloud_array):
     return pointcloud_array
 
 
-def to_affine_transformation_matrix(pos, rot):
-    rotation_from_quat = Rotation.from_quat([rot.x, rot.y,rot.z,rot.w])
-    rotation_matrix = rotation_from_quat.as_matrix()
+def get_transformation():
+    """
+    Computes the transormation from needed to map one triangle onto another. See:
+    https://math.stackexchange.com/questions/158538/3d-transformation-two-triangles
+    """
 
-    position = np.array([[pos.x], [pos.y],[pos.z]])
+#From zed-camera:
+#1: 0.38363, 0.11284, 0.015562
+#2: 0.45257, -0.13175, 0.059208
+#3: 0.29046, 0.064291, -0.11021
+#4: 0.36034, -0.18044, -0.068268
 
+#From ART-Pointing-Device:
+#1: -0.19867, 0.076615, -0.23578
+#2: -0.23415, 0.32177, -0.2342
+#3: -0.043547, 0.10136, -0.23641
+#4: -0.073239, 0.34946, -0.23872
+
+    # These points are selected in RVIZ, first select 
+    # the numbered points on the marker "KIRURC Zed Mini -> ART"
+    # in correct order from the Zed-Mini Camera point cloud.
+    # Then use the ART pointing device to point at the same points.
+    # Enter the values (x,y,z) into the cam_# (ZED-cam) and
+    # art_# (ART pointing device) in order.
+
+    # From zed-camera:
+    cam_1 = np.array([0.45257, -0.13175, 0.059208])
+    cam_2 = np.array([0.38363, 0.11284, 0.015562])
+    cam_3 = np.array([0.36034, -0.18044, -0.068268])
+
+    # From ART-Pointing-Device:
+    art_1 = np.array([-0.23415, 0.32177, -0.2342])
+    art_2 = np.array([-0.19867, 0.076615, -0.23578])
+    art_3 = np.array([-0.073239, 0.34946, -0.23872])
+
+    c = (cam_1 + cam_2 + cam_3)/3.0
+    z = (art_1 + art_2 + art_3)/3.0
+
+    # Centered
+    ycam_1 = cam_1 - c
+    ycam_2 = cam_2 - c
+    ycam_3 = cam_3 - c
+
+    yart_1 = art_1 - z
+    yart_2 = art_2 - z
+    yart_3 = art_3 - z
+
+    # Build outer products
+    y1 = np.outer(ycam_1, yart_1)
+    y2 = np.outer(ycam_2, yart_2)
+    y3 = np.outer(ycam_3, yart_3)
+
+    H = y1 + y2 + y3
+
+    # Compute singular value decomposition
+    U, s, Vh = linalg.svd(H)
+
+    # Compute the rotation matrix
+    R = Vh.T.dot(U.T)
+
+    # Compute the translation
+    t = z - R.dot(c)
+    t = np.array([[t[0]], [t[1]], [t[2]]])
+    
     affine_append = np.array([[0,0,0,1]])
-
-    affine_matrix = np.append(rotation_matrix, position, axis=1)
+    affine_matrix = np.append(R, t, axis=1)
     affine_matrix = np.append(affine_matrix, affine_append, axis=0)
+    
+    return affine_matrix
 
-    return affine_matrix, rotation_matrix, position
-
-def to_inverse_affine_transformation_matrix(pos, rot):
-    rotation_from_quat = Rotation.from_quat([rot.x, rot.y,rot.z,rot.w])
-    rotation_matrix = rotation_from_quat.as_matrix()
-    inv_rotation_matrix = linalg.inv(rotation_matrix)
-
-
-    position = np.array([[pos.x], [pos.y],[pos.z]])
-    inv_position = -position
-
-    affine_append = np.array([[0,0,0,1]])
-
-    affine_matrix = np.append(inv_rotation_matrix, inv_position, axis=1)
-    affine_matrix = np.append(affine_matrix, affine_append, axis=0)
-    print(affine_matrix)
-
-    return affine_matrix, inv_rotation_matrix, inv_position
+def get_identity_transformation():
+    return np.array([
+    [ 1.,         0.,         0.,          0.],
+    [ 0.,         1.,         0.,          0.],
+    [ 0.,         0.,         1.,          0.],
+    [ 0.,         0.,         0.,          1.]])
 
 
 if __name__ == '__main__':
     node = RegistrationManager()
     node.run()
-    #while not rospy.is_shutdown():
-    #    quaternion = Rotation.from_matrix(node.art_rotation).as_quat()
-    #    x = rospy.Time.now().to_sec() * math.pi
-
-    #    t.header.stamp = rospy.Time.now()
-    #    t.transform.translation.x = node.art_translation[0]
-    #    t.transform.translation.y = node.art_translation[1]
-    #    t.transform.translation.z = node.art_translation[2]
-    #    t.transform.rotation.x = quaternion[0]
-    #    t.transform.rotation.y = quaternion[1]
-    #    t.transform.rotation.z = quaternion[2]
-    #    t.transform.rotation.w = quaternion[3]
-
-    #    br.sendTransform(t)
-    #    rate.sleep()
