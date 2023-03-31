34303b6b18eab07daff597877683b3b8311d29d5
Nikolai Franke <nikolai.franke@student.kit.edu>
2023-03-31 12:32:32 +0200

    Registration points

--- scripts/register.py
+++ scripts/register.py
@@ -5,8 +5,8 @@ import geometry_msgs.msg
 import numpy as np
 import rospy
 import tf2_ros
-
-from algos import least_squares_fit_affine_transform
+from algos import (least_squares_fit_affine_transform,
+                   nearest_orthogonal_affine_transform)
 
 
 def quaternion_from_matrix(matrix):
@@ -49,7 +49,7 @@ def broadcast_transform(M):
 
     transform.header.stamp = rospy.Time.now()
     transform.header.frame_id = "panda_link0"
-    transform.child_frame_id = "zedm_left_camera_frame"
+    transform.child_frame_id = "cam_frame"
 
     quat = quaternion_from_matrix(M)
 
@@ -73,22 +73,35 @@ if __name__ == "__main__":
 
     cam = np.array(
         [
-            [0.31358, -0.18482, 0.019107],
-            [0.33351, 0.033511, -0.022393],
-            [0.25871, -0.028601, -0.030549],
-            [0.40935, -0.040489, -0.13591],
+            [0.001747, -0.050412, 1.4198],  # 1
+            [-0.21066, 0.046029, 1.451],  # 2
+            [-0.25285, -0.082485, 1.3667],  # 3
+            # [-0.087686, -0.15671, 1.3412],  # 4
+            [-0.1308, 0.14391, 1.5322],  # 5
+            [-0.33677, 0.11442, 1.4877],  # 6
+            # [-0.33488, 0.36026, 1.6446],  # 7
+            [-0.33607, -0.18611, 1.2999],  # 8
+            [0.018436, -0.23493, 1.3151],  # 9
+            # [0.10631, 0.21223, 1.6082],  # 10
         ]
     )
 
     panda = np.array(
         [
-            [0.38616, 0.27147, 0.2776],
-            [0.53905, 0.11011, 0.25714],
-            [0.54555, 0.20716, 0.27335],
-            [0.48835, 0.14241, 0.11909],
+            [0.61752, -0.022722, 0.072349],  # 1
+            [0.40842, -0.13664, 0.071443],  # 2
+            [0.37134, 0.01915, 0.069596],  # 3
+            # [0.52641, 0.10549, 0.069],  # 4
+            [0.48214, -0.25727, 0.062706],  # 5
+            [0.27348, -0.22388, 0.064509],  # 6
+            # [0.27147, -0.50161, 0.061842],  # 7
+            [0.24772, 0.13682, 0.06017],  # 8
+            [0.61575, 0.19313, 0.066029],  # 9
+            # [0.69824, -0.32347, 0.070483],  # 10
         ]
     )
-    M = least_squares_fit_affine_transform(cam, panda)
+    # M = least_squares_fit_affine_transform(cam, panda)
+    M = nearest_orthogonal_affine_transform(cam, panda)
     print(M)
     broadcast_transform(M)
     # broadcast_transform(R, t)
